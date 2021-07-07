6c77a5acd545a4ecf2242d36c506c5efa2f8bfa5
webroot-ua <86402619+webroot-ua@users.noreply.github.com>
2021-07-07 13:09:25 +0300

    Add files via upload

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -5,7 +5,7 @@ export default ({ comment }) => {
     <div className="col shadow p-3 mb-3 bg-body rounded">
       <div className="modal__comment d-flex justify-content-between">
         <p>{new Date(Date.now()).toDateString()}</p>
-        <p>{comment.name}1</p>
+        <p>{comment.name}</p>
       </div>
       {comment.comment}
     </div>
