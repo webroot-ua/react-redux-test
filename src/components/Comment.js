cea0ddf3a67419ea8ea51af1dcd72fe0513c496c
webroot-ua <86402619+webroot-ua@users.noreply.github.com>
2021-07-07 12:52:17 +0300

    Add files via upload

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 export default ({ comment }) => {
   return (
-    <div className="col mb-3">
-      <p>18.12.2019</p>
+    <div className="col shadow p-3 mb-3 bg-body rounded">
+      <div className="modal__comment d-flex justify-content-between">
+        <p>18.12.2019</p>
+        <p>{comment.name}1</p>
+      </div>
       {comment.comment}
     </div>
   )
